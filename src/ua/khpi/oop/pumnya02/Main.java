6d783ee2781e44d0b898221d6b43fbb19fec9d7e
Alex <45077387+almightypro@users.noreply.github.com>
2019-09-13 21:54:14 +0300

    Add files via upload

---
+++ src/ua/khpi/oop/pumnya02/Main.java
@@ -0,0 +1,34 @@
+package ua.khpi.oop.pumnya02;
+
+import java.awt.List;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(Integer.MAX_VALUE);
+        ArrayList<Integer> minNums = new ArrayList<Integer>();
+        findMinNums(number, minNums);
+        printInfo(number, minNums);
+    }
+
+    private static void findMinNums(int number, ArrayList<Integer> array) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        while (number > 0) {
+            arr.add(0, number % 10);
+            number /= 10;
+        }
+        int min = Collections.min(arr);
+        for(int i = 0; i < arr.size(); i++)
+            if(arr.get(i) == min) array.add(i);
+    }
+
+    private static void printInfo(int number, ArrayList<Integer> array) {
+        System.out.format("Input: %d%n", number);
+        System.out.print("Output: ");
+        for(int i : array) {
+            System.out.format("%d ", i);
+        }
+    }
+}
