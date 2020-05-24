e8445d67378bbb8c83760dd4d095876fc6fc8d3b
almightypro <sano.pumnya@gmail.com>
2020-05-25 00:20:58 +0300

    #1

---
+++ src/ua/khpi/oop/pumnya11/Pumnya11.java
@@ -0,0 +1,15 @@
+package labs.pumnya11;
+
+public class Pumnya11 {
+    private Pumnya11() {
+    }
+
+    public static void main(String[] args) {
+        /**
+         * Решение прикладной задачи данной роботы
+         * уже были вшиты в предыдущей (10-й) работе.
+         *
+         */
+        System.out.println("Hello World! =D");
+    }
+}
